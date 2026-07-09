7629cb7f35d7f626a9ee65534563589d01a78d2d
LucasAmarilla93 <lucas.amarilla.93@gmail.com>
2026-07-08 21:07:05 -0300

    Env ApiUrl

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -1,3 +1,6 @@
-//Produccion
+//Produccion es la de nest en vercel.
+//Ojo con el tema de la barra. Si lo codeo asi siempre tengo que poner / en cada peticion -> '/publicaicones'
 
-export const environment = {};
+export const environment = {
+    apiUrl: 'amarilla-lucas-tp-red-social-backen.vercel.app'
+};
